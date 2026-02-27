5216fea52e199a932534501d524551a8ee2d4054
Lee Jihee <129031670+munjji@users.noreply.github.com>
2026-02-27 15:55:26 +0900

    ⚙️setting: 프로젝트 세팅

---
+++ src/main/java/PetMoa/PetMoa/PetMoaApplication.java
@@ -0,0 +1,13 @@
+package PetMoa.PetMoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetMoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetMoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/PetMoa/PetMoa/PetMoaApplicationTests.java
@@ -0,0 +1,13 @@
+package PetMoa.PetMoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetMoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
